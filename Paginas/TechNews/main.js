cf70b450477e38c90b74b42d2f698294329750ed
MatiMorilla <MatiMorilla@users.noreply.github.com>
2021-01-07 18:24:27 -0300

    new carpets

---
+++ Paginas/TechNews/main.js
@@ -0,0 +1,11 @@
+document.querySelector(".menu-btn").addEventListener("click", function(){
+    document.querySelector(".nav-menu").classList.toggle("aparecer");
+})
+
+ScrollReveal().reveal(".banner-one",);
+ScrollReveal().reveal(".news-cards", {delay: 150});
+ScrollReveal().reveal(".banner-two", {delay: 150});
+ScrollReveal().reveal(".banner-three", {delay: 150});
+ScrollReveal().reveal(".redes", {delay: 150});
+ScrollReveal().reveal(".footer-links", {delay: 150});
+ScrollReveal().reveal(".footer", {delay: 150});
