341851f81379e1311750166021455312c012256a
MatiMorilla <MatiMorilla@users.noreply.github.com>
2021-01-19 17:08:46 -0300

    login.html y estilos

--- Paginas/TechNews/main.js
+++ Paginas/TechNews/main.js
@@ -3,9 +3,9 @@ document.querySelector(".menu-btn").addEventListener("click", function(){
 })
 
 ScrollReveal().reveal(".banner-one",);
-ScrollReveal().reveal(".news-cards", {delay: 150});
-ScrollReveal().reveal(".banner-two", {delay: 150});
-ScrollReveal().reveal(".banner-three", {delay: 150});
-ScrollReveal().reveal(".redes", {delay: 150});
-ScrollReveal().reveal(".footer-links", {delay: 150});
-ScrollReveal().reveal(".footer", {delay: 150});
+ScrollReveal().reveal(".news-cards", {delay: 70});
+ScrollReveal().reveal(".banner-two", {delay: 70});
+ScrollReveal().reveal(".banner-three", {delay: 70});
+ScrollReveal().reveal(".redes", {delay: 70});
+ScrollReveal().reveal(".footer-links", {delay: 70});
+ScrollReveal().reveal(".footer", {delay: 70});
